b628e4096343a01d7d33187731bbf1cf71223dfa
CaseyCameron <mcasey.cameron@gmail.com>
2021-11-29 21:07:59 -0800

    Header Pathing loose implementation

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import { useLocation } from 'react-router-dom';
+import { Wrapper } from '../UI';
 
 const Header = () => {
-  return (
-    <header class='flex justify-between p-4 border-b-2 border-black'>
-      Header: Campaign Companion
-    </header>
-  )
-}
+  const { pathname } = useLocation();
+  let location = pathname;
 
-export default Header
+  if (pathname === '/') location = 'campaigns';
+
+  return (
+    <Wrapper class="flex justify-between p-4 border-b-2 border-black">
+      <div>{`Campaign Companion > ${location}`}</div>
+    </Wrapper>
+  );
+};
+
+export default Header;

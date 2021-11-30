217f803458d85f97ff7c0b2ccec5c1bad7928e21
CaseyCameron <mcasey.cameron@gmail.com>
2021-11-29 20:28:50 -0800

    Campaign Detail setup

---
+++ src/views/detail/CampaignDetail.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import { getCampaignNpcs } from '../../services/routes/routes';
+import Loading from '../../components/loading/Loading';
+import NpcCard from '../../components/layout/npcs/NpcCard.js';
+import { useParams } from 'react-router-dom';
+import { useNpcs } from '../../contexts/CampaignProvider';
+import { Wrapper } from '../../components/UI';
+
+const CampaignDetail = () => {
+  const [loading, setLoading] = useState(true);
+  const { npcs, setNpcs } = useNpcs();
+  const { id } = useParams();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await getCampaignNpcs(id);
+      setNpcs(res);
+      setLoading(false);
+    };
+    fetchData();
+  }, [id]);
+
+  if (loading) return <Loading />;
+  return (
+    <Wrapper class={wrapperStyle}>
+      {npcs.map((npc) => {
+        return <NpcCard {...npc} key={npc.name} />;
+      })}
+    </Wrapper>
+  );
+};
+
+export default CampaignDetail;
+
+const wrapperStyle = `
+  flex
+  flex-wrap
+  m-2
+`;

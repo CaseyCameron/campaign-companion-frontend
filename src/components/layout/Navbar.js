125740b65d73070c6ff1a77a51fb100f7cd0639d
CaseyCameron <mcasey.cameron@gmail.com>
2021-11-29 19:53:43 -0800

    Navbar changes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,24 +1,48 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom';
+import { Wrapper } from '../UI';
 
 const Navbar = () => {
-  return (
-    <>
-    <div class={style}>
-      <li class='m-2'>
-        <Link to='/'>Campaigns</Link>
-      </li>
-      <li class='m-2'>
-        <Link to='/npcs'>Npcs</Link>
-      </li>
-    </div>
-    </>
-  )
-}
+  const location = useLocation();
+  console.log(location.pathname);
 
-export default Navbar
+  return (
+    <Wrapper class={wrapperStyle}>
+      <div class={style}>
+        <li class={leftItemStyle}>
+          <Link to="/">Campaigns</Link>
+        </li>
+        <li class={leftItemStyle}>
+          <Link to="/npcs">Npcs</Link>
+        </li>
+        {location.pathname === '/' && (
+          <div class={rightItemStyle}>Add Campaign</div>
+        )}
+        {location.pathname === '/npcs' && (
+          <div class={rightItemStyle}>Add Npc</div>
+        )}
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Navbar;
 
 const style = `
   flex
   list-none
+`;
+
+const leftItemStyle = `
+  m-2 
+`;
+
+const rightItemStyle = `
+flex
+absolute 
+right-0
+m-2
 `
+const wrapperStyle = `
+
+`;

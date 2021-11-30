153f2989e3d9a5826154b8ead7513ac655431d63
CaseyCameron <mcasey.cameron@gmail.com>
2021-11-29 19:54:01 -0800

    Npcs Loading implemented

--- src/views/Npcs.js
+++ src/views/Npcs.js
@@ -1,28 +1,31 @@
 import React, { useEffect, useState } from 'react'
-import Card from '../components/UI/Card'
-import NpcCard from '../components/layout/npcs/NpcCard.js'
-import { getNpcs } from '../services/routes/routes'
+import Loading from '../components/loading/Loading.js';
+import NpcCard from '../components/layout/npcs/NpcCard.js';
+import { getNpcs } from '../services/routes/routes';
 
 const Npcs = () => {
-  const [npcs, setNpcs] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [npcs, setNpcs] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
-      const res = await getNpcs()
-      setNpcs(res)
-    }
-    fetchData()
-  },[])
+      const res = await getNpcs();
+      setNpcs(res);
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
 
+  if (loading) return <Loading />;
   return (
     <>
-      <div class='m-2 flex'>
-        {npcs.map(npc => {
-            return <NpcCard {...npc} key={npc.id} />
+      <div class="m-2 flex">
+        {npcs.map((npc) => {
+          return <NpcCard {...npc} key={npc.id} />;
         })}
       </div>
     </>
-  )
-}
+  );
+};
 
 export default Npcs

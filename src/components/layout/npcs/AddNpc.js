4b3bf64c22a24a622c0711ca186e4b971b74b6e9
CaseyCameron <mcasey.cameron@gmail.com>
2022-01-14 18:30:54 -0800

    FormModal Refactor and SearchBar adjustment

---
+++ src/components/layout/npcs/AddNpc.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Button from '@material-ui/core/Button';
+import FormModalDialog from '../../modals/FormModalDialog';
+
+const AddNpc = ({ open, handleOpen, handleClose }) => {
+  return (
+    <>
+      <Button class={button} type="submit" variant="outline" onClick={handleOpen}>
+        Add Npc
+      </Button>
+      <FormModalDialog open={open} handleClose={handleClose} formType={'addNpc'} />
+    </>
+  )
+}
+
+export default AddNpc;
+
+const button = `View
+  border-solid
+  border-2
+  rounded-md
+  w-auto
+  shadow-sm
+`;

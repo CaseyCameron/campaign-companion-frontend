b4fb0efe698f90a926bf78608f2550222540cf47
CaseyCameron <mcasey.cameron@gmail.com>
2022-02-01 16:23:07 -0800

    Sorting out some auth UI logic

--- src/App.js
+++ src/App.js
@@ -1,43 +1,26 @@
 import React from 'react';
-import { BrowserRouter, Navigate, Routes, Route } from 'react-router-dom';
-import { useAuth } from './contexts/AuthProvider';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Auth, Campaigns, CampaignDetail, CampaignSettings, Npcs, NpcDetail } from './views/index';
 import Navbar from './components/controls/Navbar';
 import { Header } from './components/layout/index';
 import './App.css';
 
 const App = () => {
-  const { authed } = useAuth()
-
-  const RequireAuth = ({ children }) => {
-    return authed === true
-      ? children
-      : <Navigate
-          to='/auth'
-          replace
-        />
-  }
-
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
         <Navbar />
         <Routes>
-          <Route
-            path="/"
-            element={
-              // <RequireAuth>
-                <Campaigns />
-              // </RequireAuth>
-            }
-          />
+          <Route path="/" element={<Campaigns />} />
           <Route path="/auth" element={<Auth />} />
           <Route path="/npcs" element={<Npcs />} />
-          <Route path="/npcs/detail/:id" element={<NpcDetail />}/>
+          <Route path="/npcs/detail/:id" element={<NpcDetail />} />
           <Route path="/campaigns/detail/:id" element={<CampaignDetail />} />
-          <Route path="/campaigns/settings/:id" element={<CampaignSettings />} />
+          <Route
+            path="/campaigns/settings/:id"
+            element={<CampaignSettings />}
+          />
         </Routes>
       </BrowserRouter>
     </div>
--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,18 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import formTypes from '../../data/form-types';
+import FormModalDialog from '../modals/FormModalDialog';
 import { Link, useLocation } from 'react-router-dom';
+import { useAuth } from '../../contexts/AuthProvider';
 import { Wrapper } from '../UI';
 
 const Header = () => {
+  const { user } = useAuth();
+  const [open, setOpen] = useState(false);
   const { pathname } = useLocation();
   let location = pathname;
 
   if (pathname === '/') location = 'campaigns';
 
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Wrapper class="flex justify-between p-4 border-b-2 border-black">
-      <div><Link to='/'>{'Campaign Companion'}</Link> {`> ${location}`}</div>
+      <div>
+        <Link to="/">{'Campaign Companion'}</Link> {`> ${location}`}
+      </div>
+      <div className="App">
+        {!user && (
+          <Button variant="contained" color="primary" onClick={handleOpen}>
+            Signup/Sign In
+          </Button>
+        )}
+        {user && (
+          <Button class={button} variant="outlined" onClick={handleOpen}>
+            Logout
+          </Button>
+        )}
+        <FormModalDialog
+          open={open}
+          handleClose={handleClose}
+          formType={formTypes.auth}
+        />
+      </div>
     </Wrapper>
   );
 };
 
 export default Header;
+
+const button = `
+  border-solid
+  border-2
+  rounded-md
+  w-auto
+  shadow-sm
+`;
